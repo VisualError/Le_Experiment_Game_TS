11ff1cc0fd4480c9125a23d6584fee0adc2b0f9f
Vis <adominiqjakob1225@gmail.com>
2024-02-23 00:43:00 +0800

    waaaa

--- src/shared/AbstractClasses/ModuleLoaderAbstract.ts
+++ src/shared/AbstractClasses/ModuleLoaderAbstract.ts
@@ -12,7 +12,7 @@ abstract class ModuleLoaderAbstract implements IModuleLoader {
 			if (!ModuleScript.IsA("ModuleScript")) continue;
 			try {
 				const mod = this.LoadClass(ModuleScript);
-				print(`${ModuleScript.Name} has been loaded!`);
+				print(`${ModuleScript.Name} (${mod}) has been loaded!`);
 			} catch (error) {
 				warn(`Failed to load ${ModuleScript.Name}: ${error}`);
 			}
@@ -36,19 +36,5 @@ abstract class ModuleLoaderAbstract implements IModuleLoader {
 		}
 	}
 }
-
-// /**
-//  * Get a class by name from the cache.
-//  *
-//  * @param name - The name of the class to retrieve.
-//  * @returns The class with the specified name, or undefined if the class was not found.
-//  */
-// function GetClassByName(name: string): IClass | undefined {
-//     const callerInfo = debug.info(3, "s");
-//     if (!.Classes.has(name)) {
-//       warn(`${callerInfo} tried to get nil module: ${name}`);
-//     }
-//     return Cached.Classes.get(name);
-// }
 
 export = ModuleLoaderAbstract;
